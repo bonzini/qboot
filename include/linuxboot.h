cda6fd5ebdc7f3138c1211b02f08960f71d7f2a3
Paolo Bonzini <pbonzini@redhat.com>
2015-05-21 16:19:33 +0200

    commit missing file
    reported by Kashyap Chamarty
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

---
+++ include/linuxboot.h
@@ -0,0 +1,19 @@
+#ifndef BIOS_LINUXBOOT_H
+#define BIOS_LINUXBOOT_H 1
+
+#include <stdbool.h>
+
+struct linuxboot_args {
+	/* Output */
+	void *setup_addr, *cmdline_addr, *kernel_addr, *initrd_addr;
+	uint32_t setup_size, kernel_size;
+
+	/* Input */
+        uint32_t cmdline_size, vmlinuz_size, initrd_size;
+	uint8_t header[8192];
+};
+
+bool parse_bzimage(struct linuxboot_args *args);
+void boot_bzimage(struct linuxboot_args *args);
+
+#endif
